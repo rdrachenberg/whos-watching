c3892a0c11d7216d055c0d7ae849b5c851ad64aa
Ryan Drachenberg <tssinvestments@gmail.com>
2018-10-30 14:28:42 -0400

    css adjustments

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,19 +1,39 @@
-$(".zip-submit").on("click", function (e) {
-    e.preventDefault();
+function sortById() {
+    const id = document.getElementById('id').value;
+    window.location = "/students/sort/id/" + id;
+}
 
-    var zipSearch = {
-        ZIP: $("#zip").val().trim()
-    };
+function sortByAddress() {
+    const address = document.getElementById('address').value;
+    window.location = "/students/sort/address/" + address;
+}
 
-    // Send the POST request.
-    $.ajax("/students/sort", {
-        type: "GET",
-        data: zipSearch
-    }).then(
-        function () {
-            console.log("Clicked and searched");
-            // Reload the page to get the updated list
-            location.reload();
-        }
-    );
-});
+function sortByZip() {
+    const zip = document.getElementById('zip').value;
+    window.location = "/students/sort/" + zip;
+}
+
+function sortByFristName() {
+    const first_name = document.getElementById('first_name').value;
+    window.location = "/students/sort/fname/" + first_name;
+}
+
+function sortByLasttName() {
+    const last_name = document.getElementById('last_name').value;
+    window.location = "/students/sort/lname/" + last_name;
+}
+
+function sortByState() {
+    const stateSort = document.getElementById('state_sort').value;
+    window.location = "/students/sort/state_sort/" + stateSort;
+}
+
+function sortByDob() {
+    const dob = document.getElementById('dob').value;
+    window.location = "/students/sort/dob/" + dob;
+}
+
+function sortByNotState() {
+    const state = document.getElementById('state').value;
+    window.location = "/students/sort/state/" + state;
+}
